ffd3aa7e5f6ac4be354fa5499f4207389924a1d8
DiallAbdoulaye <diallo.a6@gmail.com>
2018-05-24 16:09:00 +0200

    input login

--- routes/Home.js
+++ routes/Home.js
@@ -2,7 +2,10 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
+  TextInput,
   View,
+  Image,
+  TouchableOpacity,
   TabBarIOS,
   StatusBar,
   SafeAreaView,
@@ -13,7 +16,24 @@ class HomeScreen extends React.Component {
   render() {
     return (
       <View style={[styles.container, { backgroundColor: '#6a51ae' }]}>
-        <Text style={styles.title}>Home Screen</Text>
+        <Image
+          style={styles.stretch}
+          source={require('./gouki.jpg')}
+        />
+        <Text style={styles.title}>Login</Text>
+        <TextInput
+          style={{height: 40, borderColor: 'black', borderWidth: 2}}
+          onChangeText={(text) => this.setState({input: text})}
+          />
+        <Text style={styles.title}>Password</Text>
+        <TextInput
+          style={{height: 40, borderColor: 'black', borderWidth: 2}}
+          onChangeText={(text) => this.setState({input: text})}
+          secureTextEntry={true}
+          />
+          <TouchableOpacity onPress={this._onPressButton}>
+          <Text style={styles.title}> Sign In </Text>
+          </TouchableOpacity>
       </View>
     );
   }
@@ -24,7 +44,14 @@ const styles = StyleSheet.create({
     flex:1,
   },
   title: {
-    marginTop:250,
+    marginTop:10,
     textAlign:'center',
   },
+  stretch: {
+    width: 500,
+    height: 200,
+    alignItems: 'center',
+    justifyContent:'center'
+
+  }
 });
