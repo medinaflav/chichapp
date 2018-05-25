e54b95a06f19aea6e513b8d1bb36ace99a523bb7
FlavienMedina <flavien_94@hotmail.fr>
2018-05-25 17:28:43 +0200

    location

--- routes/Home.js
+++ routes/Home.js
@@ -21,7 +21,44 @@ const {height, width} = Dimensions.get('window');
 
 
 class LoginScreen extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      latitude: null,
+      longitude: null,
+      adress: null,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log("----------------------------");
+        console.log(position.coords.latitude);
+        console.log(position.coords.longitude);
+        console.log("----------------------------");
+          fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=AIzaSyBt_Ge6IrFP3a7YnElPGqM84xw9BBekl0Q`)
+          .then(res => res.json())
+          .then(res => {
+            var adress = res.results[0].address_components;
+            var fullAdress = `${adress[0].long_name} ${adress[1].long_name}, ${adress[2].long_name} ${adress[adress.length - 1].long_name}`
+            console.log(fullAdress);
+            console.log("----------------------------");
+            this.setState({ adress: fullAdress })
+            return;
+          })
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  }
 
   static navigationOptions = {
   tabBarIcon: ({ tintColor }) => (
@@ -33,14 +70,13 @@ class LoginScreen extends React.Component {
       <View style={{flex:1,backgroundColor:"#fff"}}>
         <View style={styles.container}>
           <View style={styles.searchSection}>
-            <TextInput style={styles.input} style={{height: 40,paddingRight:"15%",borderWidth: 0,margin:10,paddingLeft:10,fontSize:20}}
-              value={"Efreitech Villejuif, 94800"}/>
+            <TextInput style={styles.input} style={{height: 40,paddingRight:"15%",borderWidth: 0,margin:10,paddingLeft:10,fontSize:15}}
+              value={this.state.adress}/>
             <Icon style={styles.searchIcon} name='navigation' type='feather' size={20}/>
           </View>
           <ScrollView style={styles.items}>
             <View style={{flexDirection: 'column',alignItems:'center'}}>
               <TouchableOpacity style={styles.item}>
-              <Text>Filter</Text>
               </TouchableOpacity>
               <TouchableOpacity style={styles.item}>
               <Text>Filter</Text>
