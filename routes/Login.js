24c78a8b4715a63f7b43e9ff6806a2a2a538de36
FlavienMedina <flavien_94@hotmail.fr>
2018-05-25 14:57:51 +0200

    login design

--- routes/Login.js
+++ routes/Login.js
@@ -13,9 +13,14 @@ import {
   isAndroid
 } from 'react-native';
 import { Icon } from 'react-native-elements'
-
+const colors = {blue:"#133242"};
 
 class LoginScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: '',password:'' };
+  }
+
   static navigationOptions = {
   tabBarIcon: ({ tintColor }) => (
     <Icon name='home' type='feather' />
@@ -23,36 +28,42 @@ class LoginScreen extends React.Component {
   tabBarVisible: false
 };
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View style={[styles.container, { backgroundColor: '#fff' }]}>
         <Image style={styles.stretch} source={require('../public/img/chichapp2.png')}/>
 
         <TextInput style={[styles.input,{marginTop:30}]}
-          onChangeText={(text) => this.setState({input: text})}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.email}
           placeholder={"email"}
-          placeholderTextColor="133242"
+          placeholderTextColor={colors.blue}
           />
-        <TextInput style={[styles.input,{marginTop:5}]}
-          onChangeText={(text) => this.setState({input: text})}
+        <TextInput style={[styles.input,{marginTop:10}]}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.password}
           placeholder={"password"}
-          placeholderTextColor="133242"
+          placeholderTextColor={colors.blue}
           secureTextEntry={true}
           />
 
-        <TouchableOpacity onPress={this._onPressButton} style={[styles.button,{marginTop:30}]}>
+        <TouchableOpacity onPress={this._onPressButton} style={[styles.button,{marginTop:60}]}
+        onPress={() =>
+          navigate('home')
+        }>
           <Text style={styles.textButton}> Sign In </Text>
         </TouchableOpacity>
         <View style={{flexDirection: 'row',}}>
-          <View style={{borderTopColor: '#133242',borderTopWidth: 1,width:"35%",marginTop:10}}/>
-          <Text style={{paddingRight:5,paddingLeft:5}} >or</Text>
-          <View style={{borderTopColor: '#133242',borderTopWidth: 1,width:"35%",marginTop:10}}/>
+          <View style={{borderTopColor:colors.blue,borderTopWidth: 1,width:"35%",marginTop:10}}/>
+          <Text style={{paddingRight:5,paddingLeft:5,fontSize:15}} >or</Text>
+          <View style={{borderTopColor:colors.blue,borderTopWidth: 1,width:"35%",marginTop:10}}/>
         </View>
         <TouchableOpacity onPress={this._onPressButton} style={styles.button}>
           <Text style={styles.textButton}> Sign in with Facebook </Text>
         </TouchableOpacity>
-        <View style={{flexDirection: 'row',marginTop:50}}>
-          <Text style={{color: 'blue',float:'right'}} onPress={this._onPressButton}>Forgot password ?</Text>
-          <Text style={{color: 'blue'}} onPress={this._onPressButton}>New here ? Sign up</Text>
+        <View style={{flexDirection: 'row',position:'absolute',bottom:0,marginBottom:50,backgroundColor:'red',width:'90%'}}>
+          <Text style={{color:colors.blue,position:'absolute',left:0}} onPress={this._onPressButton}>Forgot password ?</Text>
+          <Text style={{color:colors.blue,position:'absolute',right:0}} onPress={this._onPressButton}>New here ? Sign up</Text>
         </View>
       </View>
     );
@@ -63,7 +74,6 @@ export default LoginScreen
 const styles = StyleSheet.create({
   container: {
     flex:1,
-    color:"#fff",
     alignItems:"center"
   },
   title: {
@@ -71,28 +81,29 @@ const styles = StyleSheet.create({
     textAlign:'center',
   },
   input:{
-    height: 40,
-    borderBottomColor: '#133242',
+    height: 50,
+    borderBottomColor:colors.blue,
     borderBottomWidth: 1,
     width:"80%",
-    color:"#fff",
     paddingLeft:5,
-    paddingBottom:25,
-    paddingTop:25
+    paddingBottom:15,
+    paddingTop:15
   },
   stretch: {
     resizeMode:"contain",
-    marginTop:40,
+    marginTop:60,
     width: 150,
     height: 150,
   },
   button: {
     alignItems: 'center',
-    padding: 10,
+    textAlign:'center',
+    padding: 20,
     marginTop:10,
     marginBottom:10,
-    backgroundColor:"#133242",
+    backgroundColor:colors.blue,
     width:"80%",
+    height:55
   },
   textButton:{
     color:"#fff"
